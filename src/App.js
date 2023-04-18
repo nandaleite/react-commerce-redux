8a90d336eb7ff52ce5cf219e31462627d821f248
Fernanda Leite <contato@nandaleite.com>
2023-04-18 13:31:11 -0300

    Clean up some unused React Hooks

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
-import { createContext, useEffect, useReducer } from "react";
+import { useEffect } from "react";
 import {
   onAuthStateChangeListener,
   createUserDocumentFromAuth,
