8ad05a70661811d367ac191130b2910672884586
Fernanda Leite <contato@nandaleite.com>
2023-04-18 13:35:36 -0300

    Refactor categories pages to use redux-thunk

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,19 +1,30 @@
 import { useSelector } from "react-redux";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
-import { selectCategoriesMap } from "../../store/categories/categories.selector";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/categories.selector";
+import { Spinner } from "../../components/spinner/spinner.component";
 
 import "./categories-preview.styles.scss";
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
-
+  const isLoading = useSelector(selectCategoriesIsLoading);
+  console.log("AAAAAAA", isLoading);
   return (
     <div className="categories-preview-container">
-      <div className="products-container">
-        {Object.keys(categoriesMap).map((title) => {
-          const products = categoriesMap[title];
-          return <CategoryPreview key={title} title={title} products={products}></CategoryPreview>;
-        })}
-      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="products-container">
+          {Object.keys(categoriesMap).map((title) => {
+            const products = categoriesMap[title];
+            return (
+              <CategoryPreview key={title} title={title} products={products}></CategoryPreview>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,12 +1,17 @@
-import { Fragment, useContext, useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/product-card.component";
-import { selectCategoriesMap } from "../../store/categories/categories.selector";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/categories.selector";
 import "./category.styles.scss";
+import { Spinner } from "../../components/spinner/spinner.component";
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -16,12 +21,16 @@ const Category = () => {
   return (
     <Fragment>
       <h2 className="title">{category.toLocaleUpperCase()}</h2>
-      <div className="category-container">
-        {products &&
-          products.map((product) => {
-            return <ProductCard key={product.id} product={product} />;
-          })}
-      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="category-container">
+          {products &&
+            products.map((product) => {
+              return <ProductCard key={product.id} product={product} />;
+            })}
+        </div>
+      )}
     </Fragment>
   );
 };
